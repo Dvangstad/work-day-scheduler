f83fcfd1729855613c4e8210f272416fa0b3fbe6
Derek Vangstad <vang0519@yahoo.com>
2021-05-15 15:51:58 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+var currentDay = moment().format('dddd, MMMM Do');
+$("#currentDay").text(currentDay)
+
+var textareaEl = $("textarea");
+var currentHour = moment().format('HH')
+
+for(i=0; i<textareaEl.length; i++){
+    if(parseInt(textareaEl[i].id) < currentHour){
+        textareaEl[i].setAttribute("class", "col-10 past")
+    }else if(parseInt(textareaEl[i].id) == currentHour){
+        textareaEl[i].setAttribute("class", "col-10 present")
+    }else {
+        textareaEl[i].setAttribute("class", "col-10 future")
+    }
+}
+
+function saveToLocal(){
+    var valueToSave = $(this).prev().val();
+    var keyToSave = $(this).prev().attr("id")
+    localStorage.setItem(keyToSave, valueToSave)
+}
+
+$("button").on("click", saveToLocal)
